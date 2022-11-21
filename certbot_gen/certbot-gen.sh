4542a95c10d1db2927a9180025a1397732316964
Han <han@hanhuang.tech>
2022-11-21 07:38:54 +0000

    Amend certbot-gen.sh to gen-certs

--- certbot_gen/certbot-gen.sh
+++ certbot_gen/certbot-gen.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-docker run -it --name certbot_gen -v ${pwd}/certs:/etc/letsencrypt -v ${pwd}/gen-certs:/gen-certs -v ${pwd}/certs.conf:/etc/nginx/conf.d/certs.conf -dp 80:80 certbot_gen
-docker exec -it certbot_gen /gen-certs/certonly.sh
-
+docker run -it --name certbot_gen -v ${PWD}/certs:/etc/letsencrypt -v ${PWD}/gen-certs:/gen-certs -v ${PWD}/certs.conf:/etc/nginx/conf.d/certs.conf -dp 80:80 certbot_gen
+docker exec -w /gen-certs certbot_gen bash certonly.sh
