85dc54539c35026c969a2656bcaf1579201a6585
Han <han@hanhuang.tech>
2023-01-01 04:53:09 +0000

    Amend conf for php

--- hht-deploy.sh
+++ hht-deploy.sh
@@ -4,5 +4,7 @@ bash ${PWD}/certbot_gen/certbot-gen.sh
 docker stop certbot_gen
 echo -e "\e[1mcerts generated, running docker-compose\e[0m"
 docker-compose up -d
+docker exec -it hht_deploy-web-1 php-fpm8 -D
+#cron tab for host machine to git pull
 echo "00 12 */1 * * su -s /bin/sh root -c 'cd /home/han/hht && /bin/git pull origin master --quiet'" | crontab -
 
