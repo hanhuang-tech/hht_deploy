3528b33537cf7777c654c82e2d024084b1de8812
Han <han@hanhuang.tech>
2023-01-13 10:49:19 +0000

    Add localcron for local cronjobs

--- hht-deploy.sh
+++ hht-deploy.sh
@@ -6,5 +6,5 @@ echo -e "\e[1mcerts generated, running docker-compose\e[0m"
 docker-compose up -d
 docker exec -it hht_deploy-web-1 php-fpm8 -D
 #cron tab for host machine to git pull
-echo "00 12 */1 * * su -s /bin/sh root -c 'cd /home/han/hht && /bin/git pull origin master --quiet' && 'truncate -s 100 /var/lib/docker/containers/*/*-json.log'" | crontab -
+cp localcron /var/spool/cron/root
 
