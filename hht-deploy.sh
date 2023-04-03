4ffdfeb981669ffc715e920367876b08b223feda
Han <han@hanhuang.tech>
2023-04-03 05:47:20 +0000

    Remove phpfpm from hht-deploy.sh, remove env

--- hht-deploy.sh
+++ hht-deploy.sh
@@ -7,8 +7,6 @@ docker stop certbot_gen
 echo -e "\e[1mcerts generated, running docker-compose\e[0m"
 #docker compose
 docker-compose up -d
-docker exec -it hht_deploy-web-1 php-fpm8 -D
-rm -rf var.env
 #cron tab for host machine to git pull
 cp localcron /var/spool/cron/root
 
