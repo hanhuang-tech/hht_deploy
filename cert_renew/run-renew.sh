ee114f83b14ae601fe1f619e516bc5b04290c32c
Han <han@hanhuang.tech>
2023-04-16 04:05:55 +0000

    Refactor bash/docker scripts and docker create volume

--- cert_renew/run-renew.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker build -t cert-renew .
-docker run -it --name cert-renew \
--v ${PWD}/../certbot_gen/certs:/etc/letsencrypt \
--d \
-cert-renew
--- hht-deploy.sh
+++ hht-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-#docker build
+#create persistent docker volume
+docker volume create certs
+#build and clean up certbot_gen
 docker build -t certbot_gen certbot_gen
 bash ${PWD}/certbot_gen/certbot-gen.sh
-#clean up certbot_gen
 docker stop certbot_gen
-echo -e "\e[1mcerts generated, running docker-compose\e[0m"
-#docker compose
+echo -e "\e[44mcertbot_gen completed and stopped, running docker-compose\e[0m"
 docker-compose up -d
 #cron tab for host machine to git pull
-cp localcron /var/spool/cron/root
+cp git-local.cron /var/spool/cron/root
 
