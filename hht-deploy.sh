1e7ac554398d493e419dada104a7832485dee445
Han <han@hanhuang.tech>
2023-04-22 03:34:28 +0000

    Add sed.sh into docker compose and script

--- hht-deploy.sh
+++ hht-deploy.sh
@@ -6,6 +6,7 @@ docker build -t certbot_gen certbot_gen
 bash ${PWD}/certbot_gen/certbot-gen.sh
 docker stop certbot_gen
 echo -e "\e[44mcertbot_gen completed and stopped, running docker-compose\e[0m"
+/bin/sh sed.sh
 docker-compose up -d
 #cron tab for host machine to git pull
 cp git-local.cron /var/spool/cron/root
