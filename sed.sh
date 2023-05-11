2c15e01879de66b088feab6c287c57fdaaa4f45a
Han <han@hanhuang.tech>
2023-05-12 11:14:28 +1200

    Amend inclusion of all .html of absolute paths in sed.sh

--- sed.sh
+++ sed.sh
@@ -2,5 +2,5 @@
 #sets environment variables in local and stream edits fields in html files
 IP4=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-sed -i s/:IP4/$IP4/ /home/han/hht/hanhuang.tech/*.html
-sed -i s/:AZ/$AZ/ /home/han/hht/hanhuang.tech/*.html
+sed -i s/:IP4/$IP4/ /home/han/hht/*/*.html
+sed -i s/:AZ/$AZ/ /home/han/hht/*/*.html
