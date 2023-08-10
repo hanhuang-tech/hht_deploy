56fd83c72be64913c4f2471b7c8f8b54eae93b5e
hanhuang <han@hanhuang.tech>
2023-08-11 00:49:57 +1200

    Modify sed.sh to include $TOKEN Metadata Service Vers 2

--- sed.sh
+++ sed.sh
@@ -3,7 +3,7 @@
 TOKEN=`curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
 IP4=`curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4`
 AZ=`curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone`
-sed -i "s/:IP4/$IP4/g" /home/han/hht/*/*html
-sed -i "s/:AZ/$AZ/g" /home/han/hht/*/*html
-sed -i "s/:IP4/$IP4/g" /home/han/hht/*/*/*html
-sed -i "s/:AZ/$AZ/g" /home/han/hht/*/*/*html
+sed -i "s/:IP4/$IP4/" /home/han/hht/*/*.html
+sed -i "s/:AZ/$AZ/" /home/han/hht/*/*.html
+sed -i "s/:IP4/$IP4/" /home/han/hht/*/*/*.html
+sed -i "s/:AZ/$AZ/" /home/han/hht/*/*/*.html
