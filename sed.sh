028cb9c03fed997d91b82194403e6040c7a6c495
root <root@ip-172-31-4-109.ap-southeast-2.compute.internal>
2025-08-02 16:27:05 +1200

    add date crontab

--- sed.sh
+++ sed.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-#sets environment variables in local and stream edits fields in html files
-TOKEN=`curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-IP4=`curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4`
-AZ=`curl -sH "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone`
-sed -i "s/:IP4/$IP4/" /home/han/hht/*/*.html
-sed -i "s/:AZ/$AZ/" /home/han/hht/*/*.html
-sed -i "s/:IP4/$IP4/" /home/han/hht/*/*/*.html
-sed -i "s/:AZ/$AZ/" /home/han/hht/*/*/*.html
+# sets environment variables in local and stream edits fields in html files
+TOKEN='curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"'
+IP4='curl -sH "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/public-ipv4"'
+AZ='curl -sH "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/placement/availability-zone"'
+sed -i "s/:IP4/$IP4/" "/home/han/hht/hanhuang.tech/index.html"
+sed -i "s/:AZ/$AZ/" "/home/han/hht/hanhuang.tech/index.html"
+sed -i "s/:IP4/$IP4/" "/home/han/hht/hanhuang.tech/index.html"
+sed -i "s/:AZ/$AZ/" "/home/han/hht/hanhuang.tech/index.html"
+echo "sed.sh"
