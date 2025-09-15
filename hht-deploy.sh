f5c224f1a7b7fd6514bfacf789dc2cc02211d4cf
Han Huang <han@hanhuang.tech>
2025-09-15 10:29:58 +0000

    remove sed.sh process on hht_deploy.sh

--- hht-deploy.sh
+++ hht-deploy.sh
@@ -10,7 +10,7 @@ docker exec -it hht_deploy-web-1 /bin/sh /scripts/certgen.sh
 docker cp web/conf.d/ hht_deploy-web-1:/etc/nginx/
 docker exec -it hht_deploy-web-1 nginx -s reload
 #add env variables to .html on frontend
-/bin/sh sed.sh
+#/bin/sh sed.sh (failing)
 #cron tab for host machine to git pull
 cp git-local.cron /var/spool/cron/root
 #start cron
