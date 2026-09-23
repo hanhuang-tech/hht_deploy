d9277813b0d30b738e002e562e68060d33713c8d
Han Huang <han@hanhuang.tech>
2026-09-23 10:59:25 +0000

    refactor cron syntax

--- sed.sh
+++ sed.sh
@@ -3,8 +3,4 @@
 TOKEN='curl -sX PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"'
 IP4='curl -sH "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/public-ipv4"'
 AZ='curl -sH "X-aws-ec2-metadata-token: $TOKEN" "http://169.254.169.254/latest/meta-data/placement/availability-zone"'
-sed -i "s/:IP4/$IP4/" "/home/han/hht/hanhuang.tech/index.html"
-sed -i "s/:AZ/$AZ/" "/home/han/hht/hanhuang.tech/index.html"
-sed -i "s/:IP4/$IP4/" "/home/han/hht/hanhuang.tech/index.html"
-sed -i "s/:AZ/$AZ/" "/home/han/hht/hanhuang.tech/index.html"
 echo "sed.sh"
